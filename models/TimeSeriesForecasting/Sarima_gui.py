26db23b53918d75f9d30876172183de3513d14ec
Florian Butsch <ga78kos>
2020-08-29 08:43:34 +0200

    added comments

--- models/TimeSeriesForecasting/Sarima_gui.py
+++ models/TimeSeriesForecasting/Sarima_gui.py
@@ -6,19 +6,12 @@ import matplotlib.pyplot as plt
 import json
 import random
 from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pmdarima as pm
-from sklearn.utils import shuffle
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, InputLayer, LSTM, Dropout, BatchNormalization, Conv1D, MaxPooling1D
-from keras.callbacks import EarlyStopping
-from keras import backend as K
 
-
-
-
+#database = feature database
+#sector = mobility or energy
+#period = 12 or 24 months
 def Sarima(database, sector, period):
     #extract target values
     Y_raw = None 
@@ -41,21 +34,23 @@ def Sarima(database, sector, period):
                 else:
                     Y_raw = pd.concat([Y_raw, new_data], axis=1)
                     
-
-
-
-    
+  
     #convert index in datetime format
     Y_raw['date'] = Y_raw.index
     Y_raw.date = pd.to_datetime(Y_raw.date).dt.to_period('m')
     Y_raw.index = Y_raw.date
     Y_raw = Y_raw.drop('date', axis=1)
     
-    #get period: 2011-01 - 2020-12
+    #get period
     y = Y_raw.loc['2011-01':'2020-12']
-    y = y[['M_Mio.tonnes_CO2']]
+    if sector == 'mobility':
+        y = y[['M_Mio.tonnes_CO2']]
+        y.columns = ['co2']
+    elif sector == 'energy':
+        y = y[['CO2 of Total Energy (Millionen Tonnen)']]
+        y.columns = ['co2']
        
-    #split data to train and prediction dataset
+    #split data to train
     if period == 12:
         y_train = y[(y.index.year <= 2019)]   
         y_test = y[(y.index.year > 2019)]
@@ -63,9 +58,7 @@ def Sarima(database, sector, period):
         y_train = y[(y.index.year <= 2018)]   
         y_test = y[(y.index.year > 2018)]
         
-    #print(y_train)
-    #print(y_test)
-    
+    #trian sarima model
     smodel = pm.auto_arima(y_train, start_p=1, start_q=1,
                             test='adf',
                             max_p=1, max_q=1, m=12,
@@ -77,13 +70,13 @@ def Sarima(database, sector, period):
 
     smodel.summary()
 
+    #get values for next months
     fitted = smodel.predict(n_periods=period, return_conf_int=False)
 
-    fit = pd.DataFrame({'M_Mio.tonnes_CO2':fitted}, index=y_test.index)
+    #add sarima prediciton to already availiable data
+    fit = pd.DataFrame({'co2':fitted}, index=y_test.index)
     Sarima_Prediction = pd.concat([y_train, fit], axis=0, join="inner")
     Sarima_Prediction.columns = ['co2']
-
-    print(Sarima_Prediction)
 
     return Sarima_Prediction
 
@@ -92,7 +85,8 @@ def Sarima(database, sector, period):
 ######################
 with open('../data/feature_database.json') as json_database:
     database = json.load(json_database)
-    
+
+#sector: mobility or energy 
 sector = 'mobility'
 Sarima_Prediction = Sarima(database, sector, period=24)
 Sarima_Prediction.plot()
